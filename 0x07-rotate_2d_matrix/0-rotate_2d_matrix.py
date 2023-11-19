d59a1684b8cf471e3a1fb23f0ec1ccaaace02c35
Peter Evance <59042622+peter-evance@users.noreply.github.com>
2023-11-20 00:30:13 +0300

    The matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,37 @@
+def rotate_2d_matrix(matrix):
+    """
+    Rotate a 2D matrix 90 degrees clockwise in place.
+
+    Parameters:
+    - matrix (list): The input 2D matrix to be rotated.
+
+    Returns:
+    - None: The matrix is modified in place.
+    """
+
+    # Check if the input is a valid 2D matrix
+    if not isinstance(matrix, list) or not matrix or not all(isinstance(row, list) for row in matrix):
+        return
+    
+    # Check if all rows have the same number of columns
+    num_columns = len(matrix[0])
+    if not all(len(row) == num_columns for row in matrix):
+        return
+    
+    rows, cols = len(matrix), num_columns
+    new_matrix = []
+
+    # Iterate through each element in the original matrix
+    for i in range(rows * cols):
+        # Add a new row if the current column index is a multiple of the number of rows
+        if i % rows == 0:
+            new_matrix.append([])
+
+        # Determine the current column (c) and row (r) indices in the original matrix
+        c, r = i % cols, rows - 1 - i // cols
+
+        # Append the corresponding element to the new matrix
+        new_matrix[-1].append(matrix[r][c])
+
+    # Update the input matrix with the rotated elements
+    matrix[:] = new_matrix
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,5 @@
+# 0x07. Rotate 2D Matrix
+
+## Learning Objective.
+
+Given an n x n 2D matrix, rotate it 90 degrees clockwise.
